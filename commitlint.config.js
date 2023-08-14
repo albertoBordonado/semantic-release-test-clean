0f7dd52b82225f6bb9ad483101367a244426dd12
albertoBordonado <albertobordonado@facephi.com>
2023-08-14 13:37:55 +0200

    docs: Activate and configurate Husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,7 +9,7 @@
 		'subject-case': [
 			2,
 			'never',
-			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+			['start-case', 'pascal-case', 'upper-case'],
 		],
 		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
